ec824c2494b34e5c31070735076f0f8985231fa0
Daniel Davis <dan@danizen.net>
2016-04-23 15:03:59 -0400

    Small fix

--- conf/settings/heroku.py
+++ conf/settings/heroku.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'project_name',
+    '{{ project_name }}',
 )
 
 MIDDLEWARE_CLASSES = (
